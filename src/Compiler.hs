060ddc6569b6e37300abfe9814ee8e87f92509ab
Andres Loeh <andres@well-typed.com>
2014-05-06 22:22:23 +0200

    Monadic compiler.

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -1,47 +1,99 @@
 {-# OPTIONS_GHC -fdefer-type-errors #-}
 module Compiler where
 
+import Control.Monad.Reader
+import Control.Monad.State
+import Control.Monad.Writer
+import Control.Monad.Error
+import Control.Monad.Identity
 import Data.Map as M
 import Data.Word
 import LLVM.General.AST as T
 import LLVM.General.AST.Constant as C
+import LLVM.General.AST.CallingConvention as T
 import LLVM.General.AST.Float as T
 import Syntax as S
 
 type Message = String
 
-compileExpr :: Expr                -- ^ expression to compile
-            -> Map S.Name Operand  -- ^ known symbols
-            -> Word                -- ^ name counter
-            -> Either Message
-                 ( Operand             -- ^ reference to result
-                 , Word                -- ^ new name counter
-                 , [Named Instruction] -- ^ list of generated instructions
-                 )
-compileExpr e symbols counter = case e of
-  S.Float d         -> Right (ConstantOperand (C.Float (Double d)), counter, [])
-  S.BinOp op e1 e2  -> case compileExpr e1 symbols counter of
-                         Left err -> Left err
-                         Right (o1, counter1, instrs1) ->
-                           case compileExpr e2 symbols counter1 of
-                             Left err -> Left err
-                             Right (o2, counter2, instrs2) ->
-                               let
-                                 newname = UnName counter2
-                                 newref  = LocalReference newname
-                                 opinstr = case op of
-                                             Plus   -> T.FAdd o1 o2 []
-                                             Minus  -> T.FSub o1 o2 []
-                                             Times  -> T.FMul o1 o2 []
-                                             Divide -> T.FDiv o1 o2 []
-                                 nmdinstr = newname := opinstr
-                               in Right (newref, counter2 + 1, instrs1 ++ instrs2 ++ [nmdinstr])
+type ExprM =
+  ReaderT (Map S.Name Operand) (
+  StateT Word (
+  WriterT [Named Instruction] (
+  ErrorT Message
+    Identity)))
 
--- (%13, [%13 = fadd %7 %10])
-                                 
+runExprM :: ExprM a -> Either Message (a, [Named Instruction])
+runExprM m = runIdentity $
+             runErrorT $
+             runWriterT $
+             flip evalStateT 0 $
+             flip runReaderT empty $ 
+             m
+
+
+incrCounter :: ExprM Word
+incrCounter = do
+  ctr <- get
+  put (ctr + 1)
+  return ctr
+
+compileExpr' :: Expr -> ExprM Operand
+compileExpr' e = case e of
+  S.Float d        -> return (ConstantOperand (C.Float (Double d)))
+  S.BinOp op e1 e2 -> do
+                        o1 <- compileExpr' e1
+                        o2 <- compileExpr' e2
+                        ctr <- incrCounter
+                        let
+                          newname = UnName ctr
+                          newref  = LocalReference newname
+                          instr   = newname := compileOp op o1 o2 []
+                        tell [instr]
+                        return newref
+  S.Var n           -> do
+                         symbols <- ask
+                         case M.lookup n symbols of
+                           Nothing -> throwError "unknown variable"
+                           Just o  -> return o
+  S.Call n exprs    -> do
+                         symbols <- ask
+                         case M.lookup n symbols of
+                           Nothing -> throwError "unknown functions"
+                           Just o  -> do
+                             os  <- mapM compileExpr' exprs
+                             ctr <- incrCounter
+                             let
+                               newname = UnName ctr
+                               newref  = LocalReference newname
+                             tell [newname := call o os]
+                             return newref
+                              
+
+call :: Operand -> [Operand] -> Instruction
+call x rs =
+  T.Call
+    False  -- never a tail call
+    C      -- C calling convention
+    []
+    (Right x)
+    (zip rs (repeat []))
+    []
+    []
+
+
+compileExpr e symbols counter = case e of
   S.Var n           -> case M.lookup n symbols of
                          Nothing -> Left "unknown variable"
                          Just o  -> Right (o, counter, [])
   S.Call n expres   -> case M.lookup n symbols of
                          Nothing -> Left "unknown function"
                          Just o  -> undefined
+
+compileOp :: Op -> Operand -> Operand -> InstructionMetadata -> Instruction
+compileOp op = case op of
+  Plus   -> T.FAdd
+  Minus  -> T.FSub
+  Times  -> T.FMul
+  Divide -> T.FDiv
+
